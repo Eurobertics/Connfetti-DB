b7b7370fb4698a33e195bf30700d5b188e39e0a2
Bernd Robertz <brobertz.net@gmail.com>
2019-10-14 16:45:03 +0200

    Version bumb

--- src/Adapter/Adapter.php
+++ src/Adapter/Adapter.php
@@ -7,7 +7,7 @@ use Connfetti\Db\Exception\QueryException;
 
 class Adapter
 {
-    public static $VERSION = '0.9.7';
+    public static $VERSION = '0.9.8';
 
     const QUERY_EXECUTE = 0;
     const QUERY_AS_STRING = 1;
