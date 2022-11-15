a13deae71f26011365612c51eb1b01d514900ef3
Bernd Robertz <10088071+Eurobertics@users.noreply.github.com>
2022-11-15 21:31:45 +0100

    Update to PHP version 8.1

--- src/Adapter/Adapter.php
+++ src/Adapter/Adapter.php
@@ -7,7 +7,7 @@ use Connfetti\Db\Exception\QueryException;
 
 class Adapter
 {
-    public static $VERSION = '0.9.9RC1';
+    public static $VERSION = '0.9.9RC2';
 
     const QUERY_EXECUTE = 0;
     const QUERY_AS_STRING = 1;
